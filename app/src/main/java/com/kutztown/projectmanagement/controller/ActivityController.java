5b575dad72bf55b2d66f61623390570a85f6e90b
Steve <steven.p.gantz@gmail.com>
2016-01-28 16:46:36 -0500

    Created files, altered build, added graphing library

---
+++ app/src/main/java/com/kutztown/projectmanagement/controller/ActivityController.java
@@ -0,0 +1,11 @@
+package com.kutztown.projectmanagement.controller;
+
+/**
+ * Created by Steven Gantz on 1/26/2016.
+ *
+ * This class controls which activity is being brought to
+ * the forefront next.
+ */
+public class ActivityController {
+
+}
---
+++ app/src/main/java/com/kutztown/projectmanagement/graphing/PieGraph.java
@@ -0,0 +1,16 @@
+package com.kutztown.projectmanagement.graphing;
+
+// Import library
+
+/**
+ * Created by Steven Gantz on 1/28/2016.
+ *
+ * This class will encapsulate the creation
+ * of a pie graph using the open source library
+ * https://github.com/PhilJay/MPAndroidChart .
+ *
+ * It is added to the project using Gradle.
+ */
+public class PieGraph {
+
+}
