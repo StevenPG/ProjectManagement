500548c2395c0847f2a41bd62a2d720b15b7d85c
Steve <steven.p.gantz@gmail.com>
2016-02-11 19:46:49 -0500

    Created the other entries

---
+++ app/src/main/java/com/kutztown/projectmanagement/data/ProgressTableEntry.java
@@ -0,0 +1,13 @@
+package com.kutztown.projectmanagement.data;
+
+/**
+ * @author Steven Gantz
+ * @date 2/11/2016
+ *
+ * This object will contain everything required to have a full
+ * entry in the progress table. Each resource can be accessed, and
+ * it can be transformed into a get string to be passed to
+ * the web service.
+ */
+public class ProgressTableEntry {
+}
--- app/src/main/java/com/kutztown/projectmanagement/data/TaskTableEntry.java
+++ app/src/main/java/com/kutztown/projectmanagement/data/TaskTableEntry.java
@@ -1,6 +1,5 @@
 package com.kutztown.projectmanagement.data;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /**
