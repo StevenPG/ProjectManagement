d1a0d606a89eadf08d5306da8f49fb7dd46dbd88
Steve <steven.p.gantz@gmail.com>
2016-02-24 12:18:55 -0500

    Successfully implemented FR20

--- app/src/main/java/com/kutztown/projectmanagement/activity/MemberList.java
+++ app/src/main/java/com/kutztown/projectmanagement/activity/MemberList.java
@@ -1,28 +1,37 @@
 package com.kutztown.projectmanagement.activity;
 
+import android.app.Activity;
 import android.app.ListActivity;
-import android.content.CursorLoader;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.app.AppCompatCallback;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.view.ActionMode;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ListAdapter;
-import android.widget.SimpleCursorAdapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.kutztown.project.projectmanagement.R;
+import com.kutztown.projectmanagement.controller.ActivityController;
 import com.kutztown.projectmanagement.data.ApplicationData;
+import com.kutztown.projectmanagement.data.ProjectTableEntry;
+import com.kutztown.projectmanagement.network.HTTPHandler;
 
-public class MemberList extends ListActivity implements AppCompatCallback {
+import java.util.ArrayList;
+
+public class MemberList extends Activity implements AppCompatCallback {
+
+    /**
+     * List of projects
+     */
+    ArrayList<String> memberList = null;
+    ListView projectView = null;
 
     @Nullable
     @Override
@@ -52,15 +61,48 @@ public class MemberList extends ListActivity implements AppCompatCallback {
         ApplicationData.delegate.getSupportActionBar().setDisplayShowHomeEnabled(true);
         ApplicationData.delegate.getSupportActionBar().setDisplayShowTitleEnabled(false);
 
+        // Retrieve projects of current user
+        this.memberList = getMembersFromProject();
 
-        CursorLoader loader = new CursorLoader(this, ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
-                null, null, null,null);
-              Cursor Contacts = loader.loadInBackground();
-        // geting the data from the contact list for now. it will be replace with a call to the database
-        ListAdapter adapter = new SimpleCursorAdapter(this, R.layout.name, Contacts, new String[]{
-                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME},
-                new int[]{ R.id.nameD},0);
-        setListAdapter(adapter);
+        if(this.memberList == null){
+            this.memberList = new ArrayList<>();
+        }
+
+        // Retrieve listview and add projects
+        projectView = (ListView) findViewById(R.id.memberListView);
+        ArrayAdapter<String> listAdapter = new ArrayAdapter<String>(this, R.layout.mainactivityrow, this.memberList);
+        /**projectView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                // this is not in the requirements: TODO Don't do anything when a member is clicked
+                return;
+
+
+                // Clicked Text contains the project name
+                String clickedText = (String) parent.getItemAtPosition(position);
+                // TODO - Set the ApplicationData.currentProject value
+                // Retrieve the project from the DB and store it globally
+                HTTPHandler handler = new HTTPHandler();
+                try {
+                    ApplicationData.currentProject = (ProjectTableEntry) handler.
+                            select(clickedText, "ProjectName", new ProjectTableEntry(), "ProjectTable");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Log.d("debug", ApplicationData.currentProject.writeAsGet());
+
+                // Get info and decide where to send the user if leader or not
+                String projectLeader = ApplicationData.currentProject.getLeaderList();
+                if(projectLeader.equals(ApplicationData.currentUser.getEmail())){
+                    startActivity(ActivityController.openLeaderViewActivity(getApplicationContext()));
+                } else {
+                    startActivity(ActivityController.openMemberViewActivity(getApplicationContext()));
+                }
+            }
+        });**/
+
+        projectView.setAdapter(listAdapter);
 
 
     }
@@ -80,6 +122,40 @@ public class MemberList extends ListActivity implements AppCompatCallback {
         onBackPressed();
         return true;
 
+    }
+
+    /**
+     * Retrieve the projects from the logged in user and parse them into an arraylist
+     * @return null if no projects, else arraylist of projects
+     */
+    protected ArrayList<String> getMembersFromProject(){
+
+        String projectList = ApplicationData.currentProject.getMemberList();
+        if(projectList == null){
+            projectList = "";
+        }
+
+        ArrayList memberArray = new ArrayList();
+
+        TextView header = (TextView) findViewById(R.id.header);
+        Log.d("debug", "Member List: " + projectList);
+        if("None".equals(projectList)){
+            header.setText("There are no members in this project");
+            return null;
+        }
+        else{
+            header.setText("Members");
+            String[] members = projectList.split("--");
+
+            for(String member : members){
+                // Wipe leftover python structures
+                member = member.replaceAll("u'", "");
+                member = member.replaceAll("'", "");
+                memberArray.add(member);
+            }
+        }
+
+        return memberArray;
     }
 
 }
