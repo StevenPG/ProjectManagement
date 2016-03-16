a1510bf0f2bd8688a35400a6949a7ff9b5b1b201
Steve <steven.p.gantz@gmail.com>
2016-03-16 00:27:40 -0400

    Fixed the encryption bug and "encryption

--- app/src/main/java/com/kutztown/projectmanagement/data/Encryption.java
+++ app/src/main/java/com/kutztown/projectmanagement/data/Encryption.java
@@ -1,37 +1,28 @@
 package com.kutztown.projectmanagement.data;
 
-import java.math.BigInteger;
-
 /**
  * Created by Hector on 3/14/16.
  */
 public final class Encryption {
     char character = ' ';
     String str;
-    public String encrypt (String password)
-    {
+
+    public String encrypt(String password) {
 
         StringBuilder sb = new StringBuilder();
-        // loop through each caracter in the password
+        // loop through each character in the password
         // assigns it to the stringbuilder in ascii form
         // and save the whole number in a integer
         for (int j = 0; j < password.length(); j++)
-            sb.append((int)character);
-
-        BigInteger mInt = new BigInteger(sb.toString());
-        str = mInt.toString();
-
-        return str ;
-
+            sb.append((int) password.charAt(j));
+        return sb.toString();
     }
 
-    public String descrypt (String password)
-    {
+    public String descrypt(String password) {
         StringBuilder strB = new StringBuilder();
-        for (int i = 0; i < password.length(); i++)
-        {
+        for (int i = 0; i < password.length(); i++) {
             character = password.charAt(i);
-            strB.append((char)character);
+            strB.append((char) character);
         }
         String descryptedP = strB.toString();
         return descryptedP;
